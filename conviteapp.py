4315ea66ad64aeb4716ca813384322ff8965a4cc
vitor hugo <vitorhugolaverdidossantos4@gmail.com>
2025-09-03 20:47:53 -0300

    convite

---
+++ conviteapp.py
@@ -0,0 +1,12 @@
+jonas = ('nao')
+
+if jonas == 'sim':
+    print('vou jogar bola')
+if jonas == 'nao':
+    print('nao vou jogar bola')
+
+pedro = ('sim')
+if pedro  == 'sim':
+    print('pedro  vai')
+    if pedro  == 'nao':
+        print('pedro  nao vai')
